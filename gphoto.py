88f7fc43b6aef31bf24d021fa19a328c845ec923
dan <dan@debian>
2014-03-17 10:43:56 -0400

    Changed gphoto to use popen and monitor line-by-line output. NOT TESTED

--- gphoto.py
+++ gphoto.py
@@ -8,6 +8,14 @@ from events import *
 
 exe = "gphoto2"
 
+###TODO move downloading to events.py
+class downloading(event):
+    type="downloading"
+    progress=0
+
+    def __init__(self, progress=0)
+        self.progress=progress
+
 def disableAutoOff():
 	cmd = "gphoto2 --set-config /main/settings/autopoweroff=0"
 	try:
@@ -30,31 +38,46 @@ def disableAutoOff():
 def captureAndDownload(p):
 	filename = time.strftime("%y%m%d_%H%M%S.jpg")
 	action = "--capture-image-and-download"
+    camera = "--auto-detect"
 
-	cmd = "%s %s --filename=%s"%(exe, action, filename)
+	#cmd = "%s %s --filename=%s"%(exe, action, filename)
+	cmd = [exe, camera, action, "--filename", filename]
 
-	try:
-		output = subprocess.check_output(cmd,
-			stderr=subprocess.STDOUT,
-			shell=True)
+    proc = subprocess.Popen(cmd, 
+            bufsize=1, 
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+
+    #loop over proc while it's alive!
+    while proc.poll() == None:
+
+        #get a line of text from the process, blocking.
+        line = proc.stdout.readline()
+
+        #check output strings for 'error'
+		if "error" in line.lower():
+
+            #kill the process 
+            proc.kill()
+
+            #return the error
+		    self.q.put( photo(
+                name = None,
+                message = line + str(proc.stdout.read()) + str( proc.stderr.read())
+                )
+
+            #because of error, we don't want to add any
+            #  downloadings to the queue
+            return
 
 
-		#print "(gphoto) output\n@@@@@@@\n%s\n@@@@@@@\n\n"%(output)
+		elif "downloading" in line.lower():
+		    self.q.put(photo(name=filename))
+		    self.q.put(downloading(progress=0))
 
-		if -1 == output.lower().find("error"):
+    #out of loop? Process has ended
+    self.q.put(downloading(progress=1))
 
-			#print "(gphoto) captured: \"%s\""%(filename)
-			p.name = filename
-
-		else:
-			p.name = None
-			p.message = "%s returned: \"%s\""%(exe, output)
-
-	except subprocess.CalledProcessError as cpe:
-		print "%s returned (%d): \"%s\""%(exe, cpe.returncode, cpe.output)
-
-		p.name = None
- 		p.message = "%s returned (%d): \"%s\""%(exe, cpe.returncode, cpe.output)
 
 class photoTaker(threading.Thread):
 	def __init__(self, q=None):
@@ -63,13 +86,9 @@ class photoTaker(threading.Thread):
 	
 	def run(self):
 	
-		p = photo()
-		captureAndDownload(p)
+		captureAndDownload()
 
-		if self.q:
-			self.q.put(p)
-
-def registerPhotoEvent(q=None):
+def registerPhotoEvent(q):
 	mythread = photoTaker(q)
 	mythread.start()    #start the waiter
 	return mythread
@@ -77,5 +96,5 @@ def registerPhotoEvent(q=None):
 
 if __name__ == "__main__":
 
-	#print captureAndDownload()
-	registerPhotoEvent()
+    q = Queue.queue()
+	registerPhotoEvent(q)
