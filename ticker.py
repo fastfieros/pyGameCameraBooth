496bf2bffa57925c17ce4ebbc2d7106c6b6ffca1
dan <dan@debian>
2014-03-19 15:23:58 -0400

    added alpha channels to icons

--- ticker.py
+++ ticker.py
@@ -19,7 +19,7 @@ pygame.mouse.set_visible(False)
 pygame.font.init()
 myfont      = pygame.font.SysFont("helvetica", 24)
 
-camera_img    = pygame.image.load("camera.jpg")
+camera_img    = pygame.image.load("camera.png")
 tv_img        = pygame.image.load("tv.png")
 
 start=time.time()
@@ -31,33 +31,38 @@ def loop():
         running = 0
 
 
-    screen.fill((255,255,255))
+    screen.fill((32,32,32))
+    s = pygame.Surface((1024,768), flags=pygame.SRCALPHA)
     t = time.time()
 
     label = myfont.render("time :%f"%t, 1, (255,0,0))
-    screen.blit(label, (40,40))
+    s.blit(label, (40,40))
 
     #draw circles on the bottom
     camera_end = 300
     screen_start = 675
     for i in range(6):
         circlex = camera_end - 90 + (i*90) + int((time.time() - start) * 100) % 90
-        pygame.draw.circle(screen, (200,200,200), (circlex,425), 20, 5)
+        pygame.draw.circle(s, (200,200,200), (circlex,425), 20, 5)
 
     #then draw the icons
-    screen.blit(camera_img, (10, 265))
-    screen.blit(tv_img, (600, 250))
+    s.blit(camera_img, (10, 265))
+    s.blit(tv_img, (600, 250))
 
     #then draw the lines
-    pygame.draw.line(screen, (0,0,0), (camera_end,400), (screen_start,400), 5)
-    pygame.draw.line(screen, (0,0,0), (camera_end,450), (screen_start,450), 5)
+    pygame.draw.line(s, (0,0,0), (camera_end,400), (screen_start,400), 5)
+    pygame.draw.line(s, (0,0,0), (camera_end,450), (screen_start,450), 5)
     
+    screen.blit(s, (0,0))
 
     pygame.display.flip()
 
 
 
 if __name__ == "__main__":
+
+    camera_img.convert_alpha()
+    tv_img.convert_alpha()
 
     try:
         while running:
